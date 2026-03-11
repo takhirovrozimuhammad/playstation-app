526e643fa19d25d1e125f88a3ec90e4cc9d6bb13
Tharih Ridzhan <141733123+takhirovrozimuhammad@users.noreply.github.com>
2026-03-11 22:08:11 +0500

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
