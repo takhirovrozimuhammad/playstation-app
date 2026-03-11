196153dc0bb9ec62bc8672865ac3ac572b86ef4a
Tharih Ridzhan <141733123+takhirovrozimuhammad@users.noreply.github.com>
2026-03-11 16:44:36 +0500

    Updated DashboardLayout.tsx

--- src/app/components/DashboardLayout.tsx
+++ src/app/components/DashboardLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import { Sidebar } from "./Sidebar";
+import Sidebar from "./Sidebar";
 import { Header } from "./Header";
 
 export function DashboardLayout() {
