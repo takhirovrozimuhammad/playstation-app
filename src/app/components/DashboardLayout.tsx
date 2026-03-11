5d83ea07c3bd89012adad3d1b6321955dac1589c
Tharih Ridzhan <141733123+takhirovrozimuhammad@users.noreply.github.com>
2026-03-11 20:53:33 +0500

    Updated DashboardLayout.tsx

--- src/app/components/DashboardLayout.tsx
+++ src/app/components/DashboardLayout.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router";
-import Sidebar, { SIDEBAR_MODE_KEY } from "./Sidebar";
+import Sidebar, { SIDEBAR_MODE_KEY } from "./sidebar";
 import { Header } from "./Header";
 
 type SidebarMode = "expanded" | "collapsed" | "auto";
